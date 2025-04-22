f92168779ef2417cc25587f2385dce8b5bc0a584
Khoasinhnam2003 <21082761.khoa@student.iuh.edu.vn>
2025-04-22 10:48:00 +0700

    updateDKHPService

---
+++ BE/DKHP_Service/src/main/java/fit/iuh/edu/vn/dkhp_service/configs/CorsConfig.java
@@ -0,0 +1,28 @@
+// File: src/main/java/fit/iuh/edu/vn/apigateway_service/configs/CorsConfig.java
+package fit.iuh.edu.vn.apigateway_service.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("http://localhost:3000");
+        corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedMethod("POST");
+        corsConfig.addAllowedMethod("PUT");
+        corsConfig.addAllowedMethod("DELETE");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
